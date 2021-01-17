49c1f07a7605150039c009fbc318a54740c6cee1
avardag <steklofan@gmail.com>
2021-01-17 23:38:10 +0100

    finished rendering movie

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const onInput = async (event) => {
       option.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
         searchInput.value = m.Title;
+        onMovieSelect(m);
       });
       resultsWrapper.appendChild(option);
     }
@@ -67,3 +68,52 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get(url, {
+    params: {
+      apikey,
+      i: movie.imdbID,
+    },
+  });
+  document.querySelector(".summary").innerHTML = renderMovie(response.data);
+};
+
+const renderMovie = (movieDetails) => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetails.Poster}" alt="${movieDetails.Title}">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetails.Title}</h1>
+        <h4>${movieDetails.Genre}</h4>
+        <p>${movieDetails.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetails.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+  `;
+};
